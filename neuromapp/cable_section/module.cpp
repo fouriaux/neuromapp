cf045bc0b3739b003a8d95eb71d944cffe9ea272
fouriaux <jeremy.fouriaux@epfl.ch>
2018-04-06 17:45:37 +0200

    add cable section module (analytic solution)

---
+++ neuromapp/cable_section/module.cpp
@@ -0,0 +1,32 @@
+#include "cable_section/options.h"
+
+static double V[];
+static double I[];
+
+static double t;          // to be initialized in configure
+static double delta_t;    // to be initialized in configure
+static double delta_x     // to be initialized in configure
+
+int cable_section_configure (int argc, char** argv) {
+
+
+}
+
+
+/*
+    solving 
+ 
+ 
+ 
+ */
+
+int execute () {
+// Writting I (input of other solvers)
+
+
+// Writting V (output of analytical solution to compare)
+
+
+// Virtual increment time step
+current_time += time_step;
+}
